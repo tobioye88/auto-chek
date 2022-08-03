d8bbb47dff81eb01a1d472bc949d72234d2e49a1
tobioye88 <tobioye88@gmail.com>
2022-08-03 20:00:06 +0100

    Fixed e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { HackerNewsHelper } from '../src/helpers/hacker-news/hacker-news.helper';
 import { AppModule } from './../src/app.module';
+import { mockStories, mockTestUsers } from './test.stubs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -23,11 +25,26 @@ describe('AppController (e2e)', () => {
   });
 
   it('/v1/words/top-10/in-titles-last (GET)', () => {
+    jest
+      .spyOn(HackerNewsHelper, 'getLastStories')
+      .mockResolvedValue(Promise.resolve(mockStories));
+
     return request(app.getHttpServer())
       .get('/v1/words/top-10/in-titles-last')
       .expect(200)
       .expect({
-        data: [],
+        data: [
+          ['pytorch', 1],
+          ['design', 1],
+          ['philosophy', 1],
+          ['2021', 1],
+          ['taxes', 1],
+          ['opensource', 1],
+          ['edition', 1],
+          ['you', 1],
+          ['cant', 1],
+          ['simply', 1],
+        ],
         message:
           'Top 10 most occurring words in the titles of the last 25 stories',
       });
@@ -43,11 +60,22 @@ describe('AppController (e2e)', () => {
       });
   });
   it('/v1/words/top-10/in-titles-last-600 (GET)', () => {
+    jest
+      .spyOn(HackerNewsHelper, 'getLastStories')
+      .mockResolvedValue(Promise.resolve(mockStories));
+    jest
+      .spyOn(HackerNewsHelper, 'getUsersByIds')
+      .mockResolvedValue(Promise.resolve(mockTestUsers));
+
     return request(app.getHttpServer())
       .get('/v1/words/top-10/in-titles-last-600')
       .expect(200)
       .expect({
-        data: [],
+        data: [
+          ['pytorch', 1],
+          ['design', 1],
+          ['philosophy', 1],
+        ],
         message:
           'Top 10 most occurring words in titles of the last 600 stories of users with at least 10,000 karma',
       });
